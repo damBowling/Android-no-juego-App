f0c0fc061d82afc66d63c65fd8bfef046f8a70ef
Borja <Borja@Borja-PC.mshome.net>
2012-10-11 13:45:58 +0200

    En este commit creo un splash Screen y creo tres paquetes
    com.adapters -> Contendrá los adaptadores de listas 
    com.sax.engines -> Contiene del MVC el controlador de los metodos de SAX
    com.sax.modelos -> Contiene del MVC el modelo de los métodos SAX
    
    Añaida imagen de arranque y creada la clase de login, vacia no hace nada
    nada aún

---
+++ MyBolos/src/com/damBowling/mybolos/Login.java
@@ -0,0 +1,16 @@
+package com.damBowling.mybolos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.login);
+	    // TODO Auto-generated method stub
+	}
+
+}
--- MyBolos/src/com/damBowling/mybolos/Splash.java
+++ MyBolos/src/com/damBowling/mybolos/Splash.java
@@ -1,15 +1,36 @@
 package com.damBowling.mybolos;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
 
 public class Splash extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Esto quita la action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
+        /*
+         * Se crea un Thread para contar un segundo y a continuación lanzar el siguiente activity (es decir un delay)
+         * */
+        new Thread(new Runnable() {
+			
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					Thread.sleep(1300);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Intent i = new Intent(Splash.this, Login.class);
+				startActivity(i);
+			}
+		}).start();
     }
 
     @Override
