bea70bbd7e1471f9e27391ab791f650a1a36b889
Borja <Borja@Borja-PC.mshome.net>
2012-10-11 13:20:20 +0200

    Initial Commit

---
+++ MyBolos/src/com/damBowling/mybolos/Splash.java
@@ -0,0 +1,20 @@
+package com.damBowling.mybolos;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Splash extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+}
