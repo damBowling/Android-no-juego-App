ce393b3ed92912ef6069643500d915c32afad5bd
Paco Guzman <pagudu1980@gmail.com>
2012-10-15 14:19:18 +0200

    prueba Paco 2

--- MyBolos/gen/com/damBowling/mybolos/R.java
+++ MyBolos/gen/com/damBowling/mybolos/R.java
@@ -19,23 +19,26 @@ public final class R {
         public static final int splashlogo=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f080006;
-        public static final int emailT=0x7f080003;
-        public static final int estadisticaBtn=0x7f080007;
-        public static final int imageView1=0x7f080001;
-        public static final int logrosBtn=0x7f08000a;
-        public static final int menu_settings=0x7f08000b;
-        public static final int partidasBtn=0x7f080008;
-        public static final int passT=0x7f080005;
-        public static final int reservasBtn=0x7f080009;
-        public static final int scrollView1=0x7f080000;
-        public static final int textView1=0x7f080002;
-        public static final int tv2=0x7f080004;
+        public static final int button1=0x7f080008;
+        public static final int emailT=0x7f080005;
+        public static final int estadisticaBtn=0x7f080009;
+        public static final int imageView1=0x7f080004;
+        public static final int logrosBtn=0x7f08000c;
+        public static final int menu_settings=0x7f08000d;
+        public static final int partidasBtn=0x7f08000a;
+        public static final int passT=0x7f080007;
+        public static final int reservasBtn=0x7f08000b;
+        public static final int scrollView1=0x7f080003;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080002;
+        public static final int tv2=0x7f080006;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
-        public static final int menu=0x7f030001;
-        public static final int splash=0x7f030002;
+        public static final int estadisticas=0x7f030000;
+        public static final int login=0x7f030001;
+        public static final int menu=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class menu {
         public static final int splash=0x7f070000;
@@ -44,8 +47,10 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int password=0x7f050006;
         public static final int server=0x7f050004;
         public static final int title_activity_splash=0x7f050003;
+        public static final int username=0x7f050005;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
